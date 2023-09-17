e2a0701f9e912bc787100e8531e5cdc886d34e42
Jeferson Filipi <jefersonfilipidev@gmail.com>
2023-09-17 16:30:26 -0300

    Feat: Criação da Model-Transacao.

---
+++ Projeto donuz/Model/Transacao.cs
@@ -0,0 +1,15 @@
+﻿namespace Projeto_donuz.Model
+{
+    public class Transacao
+    {
+        public string? Id { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime? Date { get; set; }
+        public TipoTransacao Tipo { get; set; }
+    }
+    public enum TipoTransacao
+    {
+        Credito,
+        Debito
+    }
+}
