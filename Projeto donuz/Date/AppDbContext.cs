d411d472adf5743249cbf1445441ff0a3b4d59f8
Jeferson Filipi <jefersonfilipidev@gmail.com>
2023-09-19 11:26:03 -0300

    feat: Criação da pasta Date

---
+++ Projeto donuz/Date/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto_donuz.Model;
+
+namespace Projeto_donuz.AppDbContex
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Cliente> Cadastros { get; set; }
+
+        public DbSet<Transacao> Transacao { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+            => optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=shared");
+
+
+    }
+}
