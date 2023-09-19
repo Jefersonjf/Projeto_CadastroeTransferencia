0efda61fd3efd375dbc1562440b76905f6764d24
Jeferson Filipi <jefersonfilipidev@gmail.com>
2023-09-19 11:30:49 -0300

    Feat: atualizações

--- Projeto donuz/Program.cs
+++ Projeto donuz/Program.cs
@@ -1,5 +1,6 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Projeto_donuz.Model;
+using Projeto_donuz.AppDbContex;
 using Projeto_donuz.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ClienteContext>(x => x.UseSqlite("Data source = Projeto donuz"));
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite("Data source = Projeto donuz"));
 builder.Services.AddScoped<IClienteRepositories, ClienteRepositories>();
 
 var app = builder.Build();
@@ -21,13 +22,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseEndpoints(Endpoint =>
-{
-Endpoint.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action/{id?}",
-    defaults: new { controller = "Home", Action = "Index" });
 
 app.UseHttpsRedirection();
 
