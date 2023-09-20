b4b2d31d0ecc3aa09134ecc3f377d6eb24290daf
Jeferson Filipi <jefersonfilipidev@gmail.com>
2023-09-19 21:46:12 -0300

    Feat: Atualização na Model

--- Projeto donuz/Model/Cliente.cs
+++ Projeto donuz/Model/Cliente.cs
@@ -4,14 +4,23 @@
     {
         public int Id { get; set; }
 
-        public string? Name { get; set; }
-        public string? CPF { get; set; }
-        public string? Endereco { get; set; }
-        public string? Telefone { get; set; }
-        public string? Email { get; set; }
-        public decimal Saldo { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public decimal? Saldo { get; set; }
         public List<Transacao> Transacaos { get; set; } = new List<Transacao>();
-    }
-       
+
+        public Cliente(string name, string cpf, string endereco, string telefone, string email, decimal saldo)
+        {
+            Name = name;
+            CPF = cpf;
+            Endereco = endereco;
+            Telefone = telefone;
+            Email = email;
+            Saldo = saldo;
+        }
+    }       
 
 }
---
+++ Projeto donuz/Model/Transferencias.cs
@@ -0,0 +1,9 @@
+﻿namespace Projeto_donuz.Model
+{
+    public class Transferencias
+    {
+        public int ClienteOrigem { get; set; }
+        public int ClienteDestino { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
