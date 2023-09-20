fa5719db6a06a8ffad399d6821668d0ea6b26e2b
Jeferson Filipi <jefersonfilipidev@gmail.com>
2023-09-19 21:45:12 -0300

    Feat: Criação da ViewModel

---
+++ Projeto donuz/ViewModel/ClienteViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Projeto_donuz.Model;
+
+namespace Projeto_donuz.NovaPasta2
+{
+    public class ClienteViewModel
+    {        
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public decimal Saldo { get; set; }
+    }
+}
+
+   
