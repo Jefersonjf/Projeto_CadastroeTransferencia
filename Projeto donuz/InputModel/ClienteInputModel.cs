8312bf76a42a078edf4432f862035fb26009c3bc
Jeferson Filipi <jefersonfilipidev@gmail.com>
2023-09-19 21:45:47 -0300

    Feat: Criação da InputModel

---
+++ Projeto donuz/InputModel/ClienteInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Projeto_donuz.InputModel
+{
+    public class ClienteInputModel
+    {
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public decimal Saldo { get; set; }
+    }
+}
