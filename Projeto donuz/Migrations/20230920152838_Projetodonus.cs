8c7ed842b3b45364c5e31c53e75d2a9f3dd4dee6
Jeferson Filipi <jefersonfilipidev@gmail.com>
2023-09-20 21:35:21 -0300

    Feat: Criação da Migrations

---
+++ Projeto donuz/Migrations/20230920152838_Projetodonus.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto_donuz.Migrations
+{
+    /// <inheritdoc />
+    public partial class Projetodonus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cadastros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    CPF = table.Column<string>(type: "TEXT", nullable: false),
+                    Endereco = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefone = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Saldo = table.Column<decimal>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cadastros", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacaos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Tipo = table.Column<int>(type: "INTEGER", nullable: false),
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacaos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transacaos_Cadastros_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Cadastros",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacaos_ClienteId",
+                table: "Transacaos",
+                column: "ClienteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transacaos");
+
+            migrationBuilder.DropTable(
+                name: "Cadastros");
+        }
+    }
+}
